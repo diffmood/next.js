a64e9a23b6f95d2dda77dc2a9a26e8b7259a7b88
diffmood <60176307+diffmood@users.noreply.github.com>
2023-11-28 19:23:40 +0300

    Update next.config.js

--- examples/cms-wordpress/next.config.js
+++ examples/cms-wordpress/next.config.js
@@ -1,7 +1,6 @@
 if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
+   define( 'GRAPHQL_JWT_AUTH_SECRET_KEY', 'RFEDSD_DED_SD' );
   `)
 }
 
